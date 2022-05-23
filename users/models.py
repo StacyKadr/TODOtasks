7cd33fbfe114a2e22efab379241b4a7a8fb97d46
StacyKadr <88447079+StacyKadr@users.noreply.github.com>
2022-05-23 20:55:59 +0200

    lesson_1. Start the project. (#1)

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class CustomUser(models.Model):
+    user_name = models.CharField('User name', max_length=64)
+    first_name = models.CharField('Name', max_length=64)
+    last_name = models.CharField('Surname', max_length=64)
+    birthday_year = models.PositiveIntegerField()
+    email = models.EmailField(unique=True)
