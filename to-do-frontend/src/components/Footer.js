a02058786ac20a4761498a1d94a8174ea4ba13f7
StacyKadr <holy_night@mail.ru>
2022-05-26 09:01:06 +0200

    lesson_2. Menu + Footer.

---
+++ to-do-frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>
+                тут должен быть адрес и прочая информация
+            </h1>
+        </footer>
+    )
+}
+
+export default Footer
