86f6bbafaa3c2cac92b28e6000ee52a24c2c895d
StacyKadr <88447079+StacyKadr@users.noreply.github.com>
2022-05-31 08:29:32 +0200

    Lesson 2 (#2)
    * lesson_2. Lesson 2 ready.

---
+++ to-do-frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <header>
+            <h1>
+                Menu
+            </h1>
+        </header>
+    )
+}
+
+export default Menu
