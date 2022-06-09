9b506aecc8fff998c12d39619a816a5275ae2be3
StacyKadr <88447079+StacyKadr@users.noreply.github.com>
2022-06-09 20:54:23 +0200

    Lesson 4  Views. Filters. Lesson 4 ready.

--- TODO/views.py
+++ TODO/views.py
@@ -1,11 +1,56 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.renderers import JSONRenderer
+from rest_framework.generics import ListAPIView, UpdateAPIView
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from .models import TODO, Project
+from users.models import CustomUser
 from .serializers import TODOModelSerializer, ProjectModelSerializer
+from users.serializers import UserModelSerializer
+from .filters import ProjectFilter, TODOFilter
+from rest_framework import filters
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 class TODOModelViewSet(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    filterset_class = TODOFilter
+    pagination_class = TODOLimitOffsetPagination
+
+    def destroy(self, request, pk=None, *args, **kwargs):
+        queryset = get_object_or_404(TODO, pk=pk)
+        queryset.is_active = False
+        queryset.save()
+        return Response(status=200)
+
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    filterset_class = ProjectFilter
+    pagination_class = ProjectLimitOffsetPagination
+
+class UserListAPIView(ListAPIView):
+    renderer_classes = [JSONRenderer]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserModelSerializer
+
+class UserUpdateAPIView(UpdateAPIView):
+    renderer_classes = [JSONRenderer]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserModelSerializer
+
+class TODOListView(generics.ListAPIView):
+    queryset = TODO.objects.all()
+    serializer_class = TODOModelSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['created', 'description']
+    ordering = ['created']
