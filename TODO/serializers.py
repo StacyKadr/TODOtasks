ad23a8d13c59a7e09310e2d8497b55d71b2bbcd4
StacyKadr <88447079+StacyKadr@users.noreply.github.com>
2022-06-30 15:25:20 +0200

    Lesson 8 (#9)
    * Lesson 8. Lesson 8 ready.

--- TODO/serializers.py
+++ TODO/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework import serializers
 from .models import Project, TODO
 from users.serializers import UserModelSerializer
 
-class ProjectModelSerializer(HyperlinkedModelSerializer):
+class ProjectModelSerializer(serializers.ModelSerializer):
     users = UserModelSerializer(many=True)
     class Meta:
         model = Project
         fields = '__all__'
-class TODOModelSerializer(HyperlinkedModelSerializer):
+
+class ProjectSerializerBase(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class TODOModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = TODO
         fields = '__all__'
