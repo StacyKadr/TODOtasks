4494109c22f1ae83f2e803c3033fb8e3a6417d87
StacyKadr <holy_night@mail.ru>
2022-07-20 23:34:50 +0200

    Lesson 12.Docker Compose.

--- TODOtasks/settings.py
+++ TODOtasks/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'TODOtasks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TODOtasks',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
